90eae0c50d2329c8c7ffec2961783c438b3b17bb
Germanken <germanken2020@outlook.com>
2024-07-30 17:38:52 -0500

    prueba 4 vercel

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 const rutasPaginas = require('./rutas/rutasPaginas');
 const connectDB = require('./conexiones/conexionBD');
